b355ee0815e915654d68d8207263338eed3ae487
Jan Kvapil <jkvapil6@gmail.com>
2021-11-14 18:18:16 +0100

    init

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex justify-center w-full outline-black">
+    <header class="flex-1">
+
+      <h1>Currency convertor</h1>
+    </header>
+    <main class="flex-1">
+      <input class="border" type="text">
+      <button>he</button>
+    </main>
+    
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
